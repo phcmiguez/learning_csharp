2d9d40fb4ac4f2a01fded41fa40ebd5121d85646
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-02-14 11:39:56 -0300

    Adicionar arquivos de projeto.

---
+++ learning_csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace learning_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
