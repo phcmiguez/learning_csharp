ac0b435a57eb8643e4a63b6a199689f2e097a2f0
Pedro Henrique Costa Miguez <pedrohcmiguez@gmail.com>
2023-02-17 13:29:12 -0300

    Dictionary

--- learning_csharp/Program.cs
+++ learning_csharp/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace learning_csharp
+﻿using System.Collections.ObjectModel;
+
+namespace learning_csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, C#!");
+            var alunos = new Dictionary<int, string>();
+            alunos.Add(1, "Pedro");
+            alunos.Add(2, "Adriele");
+            alunos.Add(3, "Théo");
+            alunos.Add(4, "Alice");
+
+            bool checkRepeated(string name, Dictionary<int, string> dict)
+            {
+                return dict.ContainsValue(name) ? true : false;
+            }
+
+            foreach (var aluno in alunos)
+            {
+                string name = aluno.ToString();
+                if(!checkRepeated(name, alunos))
+                {
+                    Console.WriteLine($"Chave => {aluno.Key} --- Valor => {aluno.Value}");
+                }
+            }
+
         }
+        
     }
 }
